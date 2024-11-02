bc118bee68777fcbf7013acdc50068f63827f4b3
sakhamreddy <sakhamreddy.m@husky.neu.edu>
2024-11-02 02:32:05 -0400

    description of project data

---
+++ description/summary.sh
@@ -0,0 +1,32 @@
+SUMMARY:
+
+Status: released
+
+Assay: small RNA-seq
+
+Biosample summary: Homo sapiens stomach tissue female adult (51 years)
+
+Biosample Type: tissue
+
+Replication type: unreplicated
+
+Description:
+
+The libraries contained in this Experiment come from tissue sub-sections of a 51 year old female's stomach tissue section obtained from GTEx. 
+They are stranded PE101 Illumina Hi-Seq RNA-Seq libraries from rRNA-depleted Total RNA < 200 nucleotides in size.
+
+Treatments: Tobacco Acid Pyrophosphatase for 1 hour
+
+Nucleic acid type: RNA
+
+RNA integrity number: 4
+
+Depleted in: rRNA
+
+Size range: <200
+
+Fragmentation methods: document reference
+
+Strand specificity: Strand-specific (forward)
+
+Platform: Illumina NextSeq 500
